701ccd5a35d46c9014a1b146c91b419e652c08b6
Shivam Chourasia <shivamchourasia15@gmail.com>
2023-01-20 06:14:54 +0530

    initial commit to deploy on vercel

--- config/db.js
+++ config/db.js
@@ -3,22 +3,9 @@ const connectDB = async () => {
   try {
     // // For Dev
     // var url = "mongodb://localhost:27017/WhatToWatch";
-    // // //For local db
-    // const conn = await mongoose.connect(url, {
-    //   //for remote, add uri in env and use MONGO_URI
-    //   useNewUrlParser: true,
-    //   useCreateIndex: true,
-    //   useUnifiedTopology: true,
-    //   useFindAndModify: false,
-    // });
+    // const conn = await mongoose.connect(url);
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      //for remote, add uri in env and use MONGO_URI
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
   } catch (err) {
     console.log(`Error: ${err.message}`);
     process.exit(1);
