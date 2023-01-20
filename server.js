ebe16260e26dddec4a696025f91c4340fd4f8796
Shivam Chourasia <shivamchourasia15@gmail.com>
2023-01-20 06:46:57 +0530

    exported app for aws serverless function

--- server.js
+++ server.js
@@ -24,13 +24,15 @@ app.use("/wtw/", suggestions);
 //Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
   });
 }
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`));
+
+module.exports = app;
