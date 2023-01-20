438853612464c4c4f5b63fb9a0f79ffddb30c856
Shivam Chourasia <shivamchourasia15@gmail.com>
2023-01-20 07:28:46 +0530

    created build file of react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./Rest.css";
+import React from "react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Footer from "./components/Footer";
